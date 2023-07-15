08c7b00e80b7551674ca7a80e2d2fccd9bf9c30e
George Aliev <gera08012329@gmail.com>
2023-07-15 10:51:04 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import NewsPage from './pages/NewsPage'
 
 function App() {
   return (
-    <h1>hi</h1>
+    <HashRouter basename='/'>
+      <Routes>
+        <Route path='/' element={<MainPage/>}/>
+        <Route path='/news' element={<NewsPage/>}/>
+      </Routes>
+    </HashRouter>
   )
 }
 
