f79b4f06f58b23422137cde4e3c8baa6369b3043
George Aliev <gera08012329@gmail.com>
2023-07-22 20:14:34 +0300

    feat: add deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { build, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  build: {
-    outDir: './build',
-    emptyOutDir: true 
-  }
+  base: "/hacker-news/"
 })
