d83e09f91f7583cf3164b3d2bf81ee09b296f003
George Aliev <gera08012329@gmail.com>
2023-07-26 19:52:30 +0300

    fix: modify vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   base: "/hacker-news/",
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notfound: resolve(__dirname, '404.html')
+      }
+    }
   }
 })
