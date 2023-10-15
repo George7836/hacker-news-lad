db1f16e979f58faa53c87cb79b934c21d0c3f680
George Aliev <gera08012329@gmail.com>
2023-10-15 16:15:36 +0300

    fix: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/hacker-news/",
+  base: '/hacker-news-lad/',
   build: {
     outDir: 'build',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        notfound: resolve(__dirname, '404.html')
-      }
-    }
-  }
-})
+        notfound: resolve(__dirname, '404.html'),
+      },
+    },
+  },
+});
